d1909d07971eb13643424152c58524c548d07923
rizonkumar <rizon.kumar.rahi@gmail.com>
2024-02-21 20:47:31 +0530

    Fixed the URL

--- src/components/dashboard/TableauView.js
+++ src/components/dashboard/TableauView.js
@@ -3,18 +3,23 @@ import TableauReport from "tableau-react";
 
 const TableauView = () => {
   const options = {
-    height: "500px",
+    height: "800px",
     width: "100%",
     hideTabs: true,
     hideToolbar: true,
   };
 
   const tableauUrl =
-    "https://prod-apnortheast-a.online.tableau.com/t/rizonrahi0639313a95/authoring/react/Dashboard1/Sheet%201#4";
+    "https://prod-apnortheast-a.online.tableau.com/t/rizonrahi0639313a95/views/react/Dashboard1?:origin=card_share_link&:embed=n";
+
   return (
-    <div>
-      <h2>Tableau Dashboard</h2>
-      <TableauReport url={tableauUrl} options={options} />
+    <div className="container mt-1">
+      {" "}
+      <h2 className="text-center mb-4">Tableau Dashboard</h2>
+      <div className="embed-responsive embed-responsive-16by9">
+        {" "}
+        <TableauReport url={tableauUrl} options={options} />
+      </div>
     </div>
   );
 };
